1f172084acf9117ece514e5dcc9658e27f32ff4e
Mario Mamdouh <mariomamdouh237@gmail.com>
2021-04-18 21:25:05 +0200

    add scroll animation

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@ function displayMovies(){
     var movieItem =``;
 
     for(let i=0 ; i<moviesList.length ; i++){
+
+        if(moviesList[i].title == undefined){
+            moviesList[i].title = moviesList[i].name
+        }
         movieItem += ` <div class="col-md-3 my-3">
         <div class="movie position-relative">
         <img class="w-100" src="https://www.themoviedb.org/t/p/w500${moviesList[i].poster_path}" alt="${moviesList[i].title} image">
@@ -38,3 +42,31 @@ function displayMovies(){
     document.getElementById('moviesContainer').innerHTML = movieItem;
 }
 
+var scrollBtn = document.querySelector('.scroll-to-top');
+
+
+window.onscroll = function () { showScrollToTop() };
+
+function showScrollToTop() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+
+$(".scroll-to-top").click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 2000);
+  });
+
+
+
+// scrollBtn.addEventListener('click' , ()=>{
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     })
+// })

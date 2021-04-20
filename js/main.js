4cab834fad550737d5cf2787a2372cf751093bad
Mario Mamdouh <mariomamdouh237@gmail.com>
2021-04-20 17:06:56 +0200

    modify js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,48 @@
-var myHttp = new XMLHttpRequest();
-
 var moviesList = [];
-
-var url = "https://api.themoviedb.org/3/trending/all/day?api_key=c657287b055c076418bfc63c59f64465";
-
-myHttp.open("GET", url);
-
-myHttp.send();
+var searchList = [];
 
 
-myHttp.addEventListener("readystatechange", () => {
-    if (myHttp.readyState == 4 && myHttp.status == 200) {
-        moviesList = JSON.parse(myHttp.response).results;
-        displayMovies(moviesList);
-    }
-});
+var allMoviesUrl = "https://api.themoviedb.org/3/trending/all/day?api_key=c657287b055c076418bfc63c59f64465";
 
-function displayMovies(array) {
+getData(allMoviesUrl , moviesList);
+
+function getData(url , array){
+    var myHttp = new XMLHttpRequest();
+
+    myHttp.open("GET", url);
+
+    myHttp.send();
+
+
+    myHttp.addEventListener("readystatechange", () => {
+        if (myHttp.readyState == 4 && myHttp.status == 200) {
+           array = JSON.parse(myHttp.response).results;
+           displayMovies(array);
+        }
+    });
+}
+
+
+
+
+function displayMovies(list) {
 
     var movieItem = ``;
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < list.length; i++) {
 
-        if (array[i].title == undefined) {
-            array[i].title = array[i].name
+        if (list[i].title == undefined) {
+            list[i].title = list[i].name
         }
         movieItem += ` <div class="col-md-3 my-3">
         <div class="movie position-relative">
-        <img class="w-100" src="https://www.themoviedb.org/t/p/w500${array[i].poster_path}" alt="${array[i].title} image">
-        <h3>${array[i].title}</h3>    
-        <p>${array[i].overview}</p>
+        <img class="w-100" src="https://www.themoviedb.org/t/p/w500${list[i].poster_path}" alt="${list[i].title} image">
+        <h3>${list[i].title}</h3>    
+        <p>${list[i].overview}</p>
         <div class="vote position-absolute py-2 font-weight-bolder">
                             <i class="fas fa-star">
                             </i>
-                            <span class="vote-num">${array[i].vote_average}</span>
+                            <span class="vote-num">${list[i].vote_average}</span>
                         </div>
         </div>
     </div>`
@@ -41,6 +50,32 @@ function displayMovies(array) {
 
     document.getElementById('moviesContainer').innerHTML = movieItem;
 }
+
+
+
+var movieSearchInput = document.getElementById('movieSearchInput');
+
+
+movieSearchInput.addEventListener('keyup', getSearchedMovies)
+
+
+function getSearchedMovies() {
+
+    let searchText = movieSearchInput.value;
+
+    var searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=c657287b055c076418bfc63c59f64465&query=${searchText}`;
+
+
+    if (searchText != '') {
+        getData(searchUrl , searchList);
+    }else{
+        
+       getData(allMoviesUrl , moviesList);
+    }
+}
+
+
+
 
 var scrollBtn = document.querySelector('.scroll-to-top');
 
@@ -61,43 +96,4 @@ $(".scroll-to-top").click(function () {
         scrollTop: 0
     }, 2000);
 });
-
-var movieSearchInput = document.getElementById('movieSearchInput');
-
-var searchList = [];
-
-movieSearchInput.addEventListener('keyup', getMovie)
-
-function getMovie() {
-
-    let searchText = movieSearchInput.value;
-
-    var searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=c657287b055c076418bfc63c59f64465&query=${searchText}`;
-
-
-    if (searchText != '') {
-        var myHttp2 = new XMLHttpRequest();
-
-
-
-        myHttp2.open("GET", searchUrl);
-
-        myHttp2.send();
-
-
-        myHttp2.addEventListener("readystatechange", () => {
-            if (myHttp2.readyState == 4 && myHttp2.status == 200) {
-               searchList = JSON.parse(myHttp2.response).results;
-               displayMovies(searchList);
-            }
-        });
-    }else{
-        displayMovies(moviesList)
-    }
-}
-
-
-
-
-
 
